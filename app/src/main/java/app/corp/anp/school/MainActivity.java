a379d5a1823478f9746fa1bed01dff081114c596
Pape Samba Ndour <papesambandour@hotmail.com>
2018-11-16 20:58:26 +0000

    page login

--- app/src/main/java/app/corp/anp/school/MainActivity.java
+++ app/src/main/java/app/corp/anp/school/MainActivity.java
@@ -1,13 +1,48 @@
 package app.corp.anp.school;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText txtLogin,txtpassword;
+    private Button btnConnection,btnInscription;
+    private String login,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtLogin = findViewById(R.id.txtLogin);
+        txtpassword = findViewById(R.id.txtPassword);
+        btnInscription = findViewById(R.id.btnInscription);
+        btnConnection = findViewById(R.id.btnConnection);
+        btnConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login = txtLogin.getText().toString();
+                password = txtpassword.getText().toString();
+                if (login.isEmpty() || password.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, getString(R.string.error_fields), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
